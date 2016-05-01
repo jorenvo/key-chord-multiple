d7a141c870cdfddf53d38af8b36ddbaa8ae757a9
Joren Van Onder <joren.vanonder@gmail.com>
2016-05-01 15:50:29 +0200

    Quiet curl

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -7,7 +7,7 @@ cd tests
 rm -rf test_results test_libs
 
 mkdir test_libs
-curl https://raw.githubusercontent.com/magnars/dash.el/master/dash.el > test_libs/dash.el
+curl -s https://raw.githubusercontent.com/magnars/dash.el/master/dash.el > test_libs/dash.el
 
 # unit tests
 emacs -nw -Q --batch -l test_libs/dash.el -l ../key-chord-multiple.el -l ../key-chord-multiple-tests.el --eval "(ert-run-tests-batch-and-exit)"
